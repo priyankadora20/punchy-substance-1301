f52034d328d7e0b122aa5ee07334dafd2df4412c
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2022-12-14 22:38:25 +0530

    clear app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Navbar from './components/navbar/Navbar';
-import {Landing} from "./pages/landing"
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Landing/>
+    
     </div>
   );
 }
