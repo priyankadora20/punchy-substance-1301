f8de6fcfd8ac586b5efe0ad778addcc256a33b14
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2022-12-14 22:36:39 +0530

    store created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import {Landing} from "./pages/landing"
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Landing/>
     </div>
   );
 }
