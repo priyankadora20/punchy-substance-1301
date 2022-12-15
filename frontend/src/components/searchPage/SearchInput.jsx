c5853b9ddcb36249c77e9efa080c3a4a8ffec37a
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2022-12-15 22:16:58 +0530

    hamburger done

--- frontend/src/components/searchPage/SearchInput.jsx
+++ frontend/src/components/searchPage/SearchInput.jsx
@@ -1,21 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import styles from "../styles/navbar.module.css"
-import { FiSearch } from 'react-icons/fi';
-const SearchPage = ({setQuery}) => {
-  const [search,setSearch]=useState("")
+import React, { useEffect, useState } from "react";
+import styles from "../styles/navbar.module.css";
+import { FiSearch } from "react-icons/fi";
+import { useDispatch } from "react-redux";
 
-  
-  
+const SearchPage = ({ setQuery, suggestion, suggvalue }) => {
+
+  const [search, setSearch] = useState("");
+  const [q,setQ]=useState("")
+  const dispatch=useDispatch()
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleClickSearchWithQ=(e)=>{
+    if(e){
+      setQ(e)
+    }
+  }
+  useEffect(()=>{
+      if(q){
+        // fetch data releted to query
+
+        
+      }
+    
+
+},[dispatch,q])
+
+  useEffect(() => {
+    // if(suggvalue){
+    //   setSearch(suggvalue)
+    // }else{
+    //   setQuery(search);
+    // }
+    setQuery(search)
+  }, [search, setQuery,suggvalue]);
   return (
     <div className={styles.searchbar}>
-    <div>
-      All category <span>{"|"}</span>
+      <div className={styles.search_text}>
+        All category <span>{"|"}</span>
+      </div>
+      <input type="text" value={search} onChange={handleChange} />
+      <span className={styles.span} onClick={()=>handleClickSearchWithQ(search)}>
+        <FiSearch />
+      </span>
+      
     </div>
-    <input type="text" />
-    <span className={styles.span}>
-      <FiSearch />
-    </span>
-  </div>
-  )
-}
-export default SearchPage
+  );
+};
+
+export default SearchPage;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,10 @@
-import { combineReducers, legacy_createStore } from "redux";
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import thunk from "redux-thunk";
 import {reducer as suggestionReducer} from "../redux/suggestionReducer/reducer"
 const rootReducer=combineReducers({
     suggestionReducer
 })
 
-const store = legacy_createStore(rootReducer)
+const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
 
 export {store}
