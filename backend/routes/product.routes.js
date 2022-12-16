336bc1df4159d8a1ded4f9af8881640029b28176
Dhanraj Jagtap <107112212+Dhanraj4198@users.noreply.github.com>
2022-12-16 11:48:04 +0530

    search done

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -5,11 +5,9 @@ const { ProductModel } = require("../models/Product.model");
 
 productRouter.get("/", async (req, res) => {
   let query = req.query;
-
-  const products = await ProductModel.find({
-    title: { $regex: `${query.q}`, $options: "i" },
-  });
-  console.log(products.length)
+  console.log(query.q);
+  const products = await ProductModel.find(query.q?{title: { $regex: `${query.q}`, $options: "i" }}:{});
+  console.log(products.length);
   res.send(products);
 });
 // productRouter.post("/add", async (req, res) => {
