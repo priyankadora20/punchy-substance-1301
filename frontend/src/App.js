375e9529ba33bb1aee9f4b6e814e2baf2c92e4c6
AshishChaudhary3639 <ashishchaudhary3639@gmail>
2022-12-19 10:18:37 +0530

    final commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
+import { AllRoutes } from './routes/allroutes';
 function App() {
   return (
     <div className="App">
-     
+      <Navbar/>
+      <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
